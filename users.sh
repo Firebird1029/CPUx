ea3ccc1ce5a3456dfeabc0920c15f034fe563892
Brandon Yee <byee20@punahou.edu>
2016-11-29 16:49:02 -1000

    Updated Firewall

--- users.sh
+++ users.sh
@@ -37,6 +37,6 @@ echo "README Users Sorted: "${readmeUsersSorted[*]} # TODO Change to debug
 # http://stackoverflow.com/questions/2150882/how-to-automatically-add-user-account-and-password-with-a-bash-script
 for useri in "${readmeUsersSorted[@]}"; do
    echo $useri
-   adduser --quiet --disabled-password --shell /bin/bash --home /home/newuser --gecos "User" $useri
+   adduser --quiet --disabled-password --shell /bin/bash --home /home/$useri --gecos "User" $useri
    echo "$useri:JasonTay1234!@" | chpasswd
 done
