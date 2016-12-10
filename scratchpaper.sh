6a3e46d688e41620b049879757ac5b1a3425ee19
Brandon Yee <byee20@punahou.edu>
2016-12-10 09:41:24 -1000

    In Competition

--- scratchpaper.sh
+++ scratchpaper.sh
@@ -1,3 +1,5 @@
-if grep -q "adsfkadjhfdlas" "/etc/lightdm/lightdm.conf"; then
-   echo "found"
+if grep -q "allow-guest" "/etc/lightdm/lightdm.conf"; then
+	 echo "Allow guest settings already exists."
+ else
+ 	echo "allow-guest=false" >> /etc/lightdm/lightdm.conf;
  fi
