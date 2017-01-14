a527445bfa675723a0133f20bceb6f53362fb7d9
jason2020 <jtay20@punahou.edu>
2017-01-14 22:53:25 +0000

    DF

---
+++ etc/etc/wordpress/config-localhost.php
@@ -0,0 +1,8 @@
+<?php
+define('FS_METHOD', 'direct');
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', 'A98ChHbfvK');
+define('DB_HOST', 'localhost');
+define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
+?>
