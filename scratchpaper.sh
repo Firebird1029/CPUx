466b4861941d25c80e38e270f75efbb19a6475b3
Brandon Yee <byee20@punahou.edu>
2017-01-14 09:06:22 -1000

    In Gold Competition

--- scratchpaper.sh
+++ scratchpaper.sh
@@ -3,7 +3,7 @@
 # apt-get install vsftpd
 # gedit /etc/vsftpd.conf
 
-computerUsersString=$(awk -F':' '$2 ~ "\$" {print $1}' /etc/shadow) # TODO does this work on ubuntu?
+computerUsersString=$(awk -F':' '$2 ~ "\$" {print $1}' /etc/shadow | tr '\n' ',') # TODO does this work on ubuntu?
 # IFS='' read -r -a computerUsers <<< "$computerUsersString"
 echo $computerUsersString
 # echo "${computerUsers[*]}"
