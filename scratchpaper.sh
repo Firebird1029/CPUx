e692d1264903eaa337042a58dd9bb4564ff85519
Brandon Yee <byee20@punahou.edu>
2017-01-14 09:33:26 -1000

    In Gold Competition

--- scratchpaper.sh
+++ scratchpaper.sh
@@ -3,14 +3,17 @@
 # apt-get install vsftpd
 # gedit /etc/vsftpd.conf
 
-computerUsersString=$(awk -F':' '$2 ~ "\$" {print $1}' /etc/shadow | tr '\n' ',')
-IFS=',' read -r -a computerUsers <<< "$computerUsersString"
+# computerUsersString=$(awk -F':' '$2 ~ "\$" {print $1}' /etc/shadow | tr '\n' ',')
+# IFS=',' read -r -a computerUsers <<< "$computerUsersString"
 
-computerAdminsString=$(getent group sudo | cut -d: -f4)
-IFS=',' read -r -a computerAdmins <<< "$computerAdminsString"
+# computerAdminsString=$(getent group sudo | cut -d: -f4)
+# IFS=',' read -r -a computerAdmins <<< "$computerAdminsString"
 
 # echo "${computerAdmins[@]}"
 # echo "${computerUsers[@]}"
+
+computerUsers=(a b c d)
+computerAdmins=(b c)
 
 tempUsersArray="${computerUsers[@]}"
 computerUsers=()
